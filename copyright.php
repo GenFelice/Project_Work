e0f73f292d3fd45eec93aea6914a9b901202348f
rejoicegweru <rejoice.gweru@enaipriminifad.org>
2026-04-30 14:48:20 +0200

    author change

--- copyright.php
+++ copyright.php
@@ -6,7 +6,7 @@
  * Version:           0.1.0
  * Requires at least: 6.8
  * Requires PHP:      7.4
- * Author:            The WordPress Contributors
+ * Author:            RejoiceG
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       copyright
